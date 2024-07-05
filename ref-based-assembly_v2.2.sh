fc41c8c54db9b65a0ceda0359ac2ec22dff81589
Majo Galarion <majogalarion@gmail.com>
2024-07-05 13:44:32 +0100

    Update ref-based-assembly_v2.2.sh

--- ref-based-assembly_v2.2.sh
+++ ref-based-assembly_v2.2.sh
@@ -32,7 +32,7 @@ prinseq="prinseq-lite.pl"
 minimap2="minimap2"
 samtools="samtools"
 bcftools="bcftools"
-medaka=".~/medaka/venv/bin/activate"
+medaka=". ~/medaka/venv/bin/activate"
 
 # Takes a non-compressed, concatenated FASTQ file
 
